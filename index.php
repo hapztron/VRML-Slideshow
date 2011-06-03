fc4e7f6ee285dc2dac42ad8f6c2fd6803dc29ff9
Hassadee Pimsuwan <silkzter@gmail.com>
2011-06-03 11:00:53 +0200

    UPGRADE! -> Can used Mode configure to show VRML/X3D scene in the same time as picture. TODO -> Implement with Image Comparison.

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>VRML-Slide</title>
+	<script type="text/javascript">
+		var path = "data/vrml_conformance.xml";
+		var mode = 1;
+	</script>
 	<link rel="shortcut icon" href="img/bs.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/vrml-slide.js"></script>
 	<script type="text/javascript" src="../inc/hp.js"></script>
+	<script type="text/javascript">
+		document.write("<title>" + pagename + " | VRML/X3D-Slide</title>");
+	</script>
 </head>
 <body onload="displayData()">
 	<div class="hp_body">
 		<script type="text/javascript">
 			document.write("<div class='hp_info-name'><h2>" + pagename + "</h2></div>");
 		</script>
+		
 		<!-- Show VRML Scene and Information of that scene from displayData(); function. -->
 		<div id="showData"></div>
-	
+		
 		<!-- Footer -->
 		<div class="hp_footer">
 			<script type="text/javascript">
--- js/vrml-slide.js
+++ js/vrml-slide.js
@@ -1,15 +1,18 @@
 /*
- *	You just edit the valiable below this line.
- *	Example:
- *	var file_url = "<your_path_and_file.xml>";
- *
+ *	Mode variable is define for slide that show only VRML/X3D Scene or show with picture.
+ *	0 -> Only VRML/X3D Scene.
+ *	1 -> VRML/X3D Scene with picture.
  */
 
-var file_url = "data/vrml_conformance.xml";
+/*
+mode = 1;
+path = "data/vrml_conformance.xml";
+*/
 
 /* ====================================================================================================================================================== */
+/* ===[ATTENTION]=DO=NOT=CHANGE=EVERYTHING=BELOW========================================================================================================= */
 /* ====================================================================================================================================================== */
-/* ====================================================================================================================================================== */
+
 if (window.XMLHttpRequest) {
 	/* Code for IE7+, Firefox, Chrome, Opera and Safari */
 	xmlhttp=new XMLHttpRequest();
@@ -17,7 +20,7 @@ if (window.XMLHttpRequest) {
 	/* Code for IE6, IE5 */
 	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 }
-xmlhttp.open("GET",file_url,false);
+xmlhttp.open("GET",path,false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML;
 /* "duration" is read default duration time to change between VRML scenes. */
@@ -31,25 +34,33 @@ x = xmlDoc.getElementsByTagName("Entry");
  *	===== Description =====
  *	This function is used to read data from XML file.
  *	It's must start at 0 for read from first data from XML file. (x[0])
- *	(For this case, Define valiable "i" for progress -> i = 0;
+ *	(For this case, Define variable "i" for progress -> i = 0;
  *
  *	===== Data Appear =====
  *	1. VRML Scene
  *	2. Information of VRML Scene that's appearing. And next 2 VRML Scenes that will appear.
  */
+
 var i = 0;
 function displayData()
 {
 	title = (x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue);
 	author = (x[i].getElementsByTagName("Author")[0].childNodes[0].nodeValue);
 	link = (x[i].getElementsByTagName("Link")[0].childNodes[0].nodeValue);
+	image = (x[i].getElementsByTagName("Image")[0].childNodes[0].nodeValue);
 	vrmldata = "<embed width='100%' height='320px' name='vrml_conformance' src='" + link + "' type='model/vrml'></embed>";
+	vrmlpicset = "<table border=0'><tr width='100%'><td width='545px' height='320px'><embed width='100%' height='100%' name='vrml_conformance' src='" + link + "' type='model/vrml'></embed></td><td width='545px' height='320px'><img src='" + image + "' width='100%' height='100%'></td></tr></table>";
 	info = "<div class='hp_info-main'>" + title + " - " + author + "</div>";
-	document.getElementById("showData").innerHTML = vrmldata + info;
+	
+	if (mode == 0) {
+		document.getElementById("showData").innerHTML = vrmldata + info;
+	} else if (mode == 1) {
+		document.getElementById("showData").innerHTML = vrmlpicset + info;	
+	}
 }
 		
 /*	
- *	setInterval()
+ *	interval()
  *	===== Description =====
  *	This function is related with displayData() function.
  *	It's used to set a time to change VRML scene to next scene and progress them.
@@ -61,6 +72,7 @@ function displayData()
  *		that contained data in "Second" that read from XML data file to "Milli Second" for using in this function.
  *	-	"$.ajaxSetup({ cache: flase });" is used to clean cache in Internet Explorer (Fix cache problem in IE).
  */
+
 var durationSecond = duration * 1000;
 $(document).ready(function() {
 	var refreshId = setInterval(function() {
